aa0e44fc2ae4090b0c916a0a19fa0cdfcf1bc16e
joojos <55354287+joojos@users.noreply.github.com>
2022-07-19 03:14:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function game(){
     let compWin = 0;
     let playerWin = 0;
+    
 
-    for(let i= 0; i<5; i++){
+
+   for(let i= 0; i<5; i++){
 
     console.log("Round #" + (i+1));
+    let n= 0;
 
     function copmuterPlay(){
         myPlay = ['Rock', 'Paper', 'Scissors'];
@@ -12,12 +15,13 @@ function game(){
         console.log('The Copmuter Chose '+ computerSelection);
     }   
 
-    const input = prompt("Please Choose: Rock, Paper or Scissors").toLowerCase();
+    function userChoice(){
+        input = prompt("Please Choose: Rock, Paper or Scissors").toLowerCase();
+        playerSelection = input.charAt(0).toUpperCase() + input.slice(1);   
+        console.log("You Chose " + playerSelection);
+    }
 
-    let playerSelection = input.charAt(0).toUpperCase() + input.slice(1);
-
-    console.log("You Chose " + playerSelection);
-
+    userChoice();
     copmuterPlay();
 
     function playRound(playerSelection, computerSelection){
@@ -58,11 +62,22 @@ function game(){
 
         } else{
             console.log("Please enter a valid value. Enter Rock, Paper or Scissors");
+            n = n + 1;
+            console.log('_________');
+
         }
 
     }
 
     playRound(playerSelection, computerSelection);
+
+    while(n > 0){
+        userChoice();
+        copmuterPlay();
+        playRound(playerSelection, computerSelection);
+        n = n-1;
+    }
+
     console.log("Your Score: " + playerWin + " Computer Score: " + compWin)
 
     }      
@@ -72,7 +87,7 @@ function game(){
     }else if(compWin < playerWin){
     console.log("The winner is the YOU!");
     }else {
-        console.log("it's a tie!");  
+        console.log("No winner. It's a tie!");  
     }
 }
 
